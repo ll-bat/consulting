cc2d81c02c528efde74d272b3a74191395593f93
ll-bat <gshatakishvili@gmail.com>
2020-09-14 19:10:17 +0400

    Updated database migrations v5

--- database/migrations/mid2_migrations/2020_09_14_150508_drop_name_from_users.php
+++ database/migrations/mid2_migrations/2020_09_14_150508_drop_name_from_users.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddRequiredToDocsTable extends Migration
+class DropNameFromUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,9 @@ class AddRequiredToDocsTable extends Migration
      */
     public function up()
     {
-        Schema::table('docs', function (Blueprint $table) {
-             $table->string("image")->default('null')->after('type');
-             $table->boolean('required')->default(false)->after('image');
+        Schema::table('users', function (Blueprint $table) {
+              $table->dropColumn('name');
+              $table->boolean('is_admin')->default(0);
         });
     }
 
@@ -26,7 +26,7 @@ class AddRequiredToDocsTable extends Migration
      */
     public function down()
     {
-        Schema::table('docs', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             //
         });
     }
