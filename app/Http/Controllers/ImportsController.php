a76317e86f7dca4b6274108d1cf82b17653eedd1
Vladimer Shatakishvili <gshatakishvili@gmail.com>
2020-11-13 11:34:34 +0400

    Updated excel reader

--- app/Http/Controllers/ImportsController.php
+++ app/Http/Controllers/ImportsController.php
@@ -12,57 +12,53 @@ use Illuminate\Http\Request;
 
 class ImportsController extends Controller
 {
-      
 
-      public function importProcess(Request $request){
-           request()->validate([
-               'process'   => 'required|mimes:csv,xls,xlsx'
-           ]);
-       
-           $data = Excel::toArray(new ProcessesImport, request('process'));      
-           
-           $reader = new ExcelReader($data);
-           $data = $reader->getData();
 
-           dd($data);
-      }
+    public function importProcess(Request $request)
+    {
 
-      public function importDanger(Request $request){
+    }
+
+    public function importDanger(Request $request)
+    {
         request()->validate([
-            'danger'   => 'required|mimes:csv,xls,xlsx'
+            'danger' => 'required'
         ]);
-    
-        $data = Excel::toArray(new ProcessesImport, request('danger'));      
-        
+
+        $extension = $request->file('danger')->getClientOriginalExtension();
+        if (!in_array($extension, explode(',', "csv,xlsx,xls"))) {
+            return back()->with('error', 'გთხოვთ ატვირთოთ ექსელის დოკუმენტი');
+        }
+
+        $data = Excel::toArray(new ProcessesImport, request('danger'));
+
         $reader = new ExcelReader($data);
         $data = $reader->getData();
 
-        $dangers  = [];
+        $dangers = [];
         $controls = [];
 
-        foreach ($data as $d){
+        foreach ($data as $d) {
 
             if (count($d) != 5) return back()->with('error', 'ინფორმაციის განლაგება ექსელის დოკუმენტში არასწორია');
 
-            if (!isset($dangers[$d[0]])){
+            if (!isset($dangers[$d[0]])) {
                 $danger = Danger::where('name', $d[0])->first();
-                if ($danger == null){
-                   $danger = Danger::create(['name' => $d[0], 'k' => $d[1]]);
-                }
-                else {
+                if ($danger == null) {
+                    $danger = Danger::create(['name' => $d[0], 'k' => $d[1]]);
+                } else {
                     $danger->update(['k' => $d[1]]);
                 }
 
                 $dangers[$d[0]] = $danger->id;
             }
 
-            if (!isset($controls[$d[2]])){
+            if (!isset($controls[$d[2]])) {
                 $control = Control::where('name', $d[2])->first();
 
-                if ($control == null){
-                   $control = Control::create(['name' => $d[2], 'k' => $d[3], 'rploss' => $d[4]]);
-                }
-                else {
+                if ($control == null) {
+                    $control = Control::create(['name' => $d[2], 'k' => $d[3], 'rploss' => $d[4]]);
+                } else {
                     $control->update(['name' => $d[2], 'k' => $d[3], 'rploss' => $d[4]]);
                 }
 
@@ -72,42 +68,47 @@ class ImportsController extends Controller
 
         // dd($dangers);
 
-        foreach ($data as $d){
-            $has = ControlDanger::where('control_id', $controls[$d[2]])->where('danger_id',$dangers[$d[0]])->first();
+        foreach ($data as $d) {
+            $has = ControlDanger::where('control_id', $controls[$d[2]])->where('danger_id', $dangers[$d[0]])->first();
 
             if ($has == null)
-              ControlDanger::create(['control_id' => $controls[$d[2]], 'danger_id' => $dangers[$d[0]]]);
-        }        
+                ControlDanger::create(['control_id' => $controls[$d[2]], 'danger_id' => $dangers[$d[0]]]);
+        }
 
         return back()->with('message', 'ოპერაცია წარმატებით დასრულდა');
-   }
+    }
 
 
-   public function importControl(Danger $danger){
+    public function importControl(Request $request, Danger $danger)
+    {
         request()->validate([
-            'control'   => 'required|mimes:csv,xls,xlsx'
+            'control' => 'required'
         ]);
 
+        $extension = $request->file('control')->getClientOriginalExtension();
+        if (!in_array($extension, explode(',', "csv,xlsx,xls"))) {
+            return back()->with('error', 'გთხოვთ ატვირთოთ ექსელის დოკუმენტი');
+        }
+
         $route = 'danger.show';
-    
-        $data = Excel::toArray(new ProcessesImport, request('control'));      
-        
+
+        $data = Excel::toArray(new ProcessesImport, request('control'));
+
         $reader = new ExcelReader($data);
         $data = $reader->getData();
 
         $controls = [];
 
-        foreach ($data as $d){
+        foreach ($data as $d) {
 
             if (count($d) != 3) return redirect()->route($route, [$danger->id])->with('error', 'ინფორმაციის განლაგება ექსელის დოკუმენტში არასწორია');
- 
-            if (!isset($controls[$d[0]])){
+
+            if (!isset($controls[$d[0]])) {
                 $control = Control::where('name', $d[0])->first();
 
-                if ($control == null){
-                   $control = Control::create(['name' => $d[0], 'k' => $d[1], 'rploss' => $d[2]]);
-                }
-                else {
+                if ($control == null) {
+                    $control = Control::create(['name' => $d[0], 'k' => $d[1], 'rploss' => $d[2]]);
+                } else {
                     $control->update(['k' => $d[1], 'rploss' => $d[2]]);
                 }
 
@@ -115,13 +116,13 @@ class ImportsController extends Controller
             }
         }
 
-        foreach ($data as $d){
-            $has = ControlDanger::where('control_id', $controls[$d[0]])->where('danger_id',$danger->id)->first();
+        foreach ($data as $d) {
+            $has = ControlDanger::where('control_id', $controls[$d[0]])->where('danger_id', $danger->id)->first();
 
             if ($has == null)
-               ControlDanger::create(['control_id' => $controls[$d[0]], 'danger_id' => $danger->id]);
-        }        
+                ControlDanger::create(['control_id' => $controls[$d[0]], 'danger_id' => $danger->id]);
+        }
 
         return redirect()->route($route, [$danger->id])->with('message', 'ოპერაცია წარმატებით დასრულდა');
-   }
+    }
 }
