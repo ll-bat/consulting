a3cbe751682c1a1f232eb2750b80a5156623ea79
Vladimer Shatakishvili <gshatakishvili@gmail.com>
2021-01-16 23:38:46 +0400

    Export switch styles and import it separately

--- resources/views/admin/blog/edit.blade.php
+++ resources/views/admin/blog/edit.blade.php
@@ -5,84 +5,7 @@
 ?>
 
 @section('header')
-    <style>
-
-
-        /* The switch - the box around the slider */
-        .switch {
-            position: relative;
-            display: inline-block;
-            width: 55px;
-            height: 27px;
-            float:right;
-        }
-
-        /* Hide default HTML checkbox */
-        .switch input {display:none;}
-
-        /* The slider */
-        .slider {
-            position: absolute;
-            cursor: pointer;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            background-color: #ccc;
-            -webkit-transition: .4s;
-            transition: .4s;
-        }
-
-        .slider:before {
-            position: absolute;
-            content: "";
-            height: 20px;
-            width: 20px;
-            left: 4px;
-            bottom: 4px;
-            background-color: white;
-            -webkit-transition: .4s;
-            transition: .4s;
-        }
-
-        input.default:checked + .slider {
-            background-color: #444;
-        }
-        input.primary:checked + .slider {
-            background-color: #2196F3;
-        }
-        input.success:checked + .slider {
-            background-color: #8bc34a;
-        }
-        input.info:checked + .slider {
-            background-color: #3de0f5;
-        }
-        input.warning:checked + .slider {
-            background-color: #FFC107;
-        }
-        input.danger:checked + .slider {
-            background-color: #f44336;
-        }
-
-        input:focus + .slider {
-            box-shadow: 0 0 1px #2196F3;
-        }
-
-        input:checked + .slider:before {
-            -webkit-transform: translateX(26px);
-            -ms-transform: translateX(26px);
-            transform: translateX(26px);
-        }
-
-        /* Rounded sliders */
-        .slider.round {
-            border-radius: 34px;
-        }
-
-        .slider.round:before {
-            border-radius: 50%;
-        }
-    </style>
+    <link rel="stylesheet" href="/css/switch.css" />
 @endsection
 
 
@@ -205,7 +128,7 @@
                   data:
                      '_token={{csrf_token()}}',
                   success: function (res) {
-                       
+
                   },
                   error: function (request, status, error) {
                       alert(request.responseText);
@@ -223,5 +146,5 @@
            @method('delete')
 
            <button class="btn btn-danger position-absolute" onclick="return confirm('ნამდვილად გსურთ ამ ბლოგის წაშლა ?')" style="border-radius: 20px; top:10px;"> წაშლა </button>
-     </form> 
+     </form>
 @endsection
