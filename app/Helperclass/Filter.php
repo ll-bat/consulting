7ee4a701698cd206f5dc317d83425b57249209cd
Vladimer Shatakishvili <gshatakishvili@gmail.com>
2021-01-18 20:48:06 +0400

    Improve code in Filter.php

--- app/Helperclass/Filter.php
+++ app/Helperclass/Filter.php
@@ -6,10 +6,12 @@ class Filter
 {
 
     private $data;
-
+    private $oldImages;
+    private $imageNameValidator = [];
 
     public function __construct($obj)
     {
+        $this->oldImages = session()->get('oldImages') ?? [];
         $this->startFiltering($obj);
     }
 
@@ -23,39 +25,104 @@ class Filter
         $newobj = [];
 
         foreach ($obj as $o) {
-            $d = $this->filter($o);
-            if (!$d) continue;
-
+            try {
+                $d = $this->filter($o);
+                if (!$d) continue;
+            } catch (\Exception $e) {
+                continue;
+            }
             $newobj[] = $d;
         }
+
+        session()->put('rule', $this->imageNameValidator);
 
         $this->data = $newobj;
     }
 
     public function filter($o)
     {
+        /**
+         * Check variables and their values. If at least one variable has invalid type or value, omit whole element...
+         */
+
         $obj = [];
         $data = [];
         $false = false;
 
-
-        if (!$this->isInt($o['did'], '', $false)) return false;
-        else {
+        /**
+         * DangerId should of type integer
+         */
+        if ($this->isInt($o['did'], '', $false)) {
             $obj['did'] = $o['did'];
         }
-        if (!$this->isInt($o['pid'], '', $false)) return false;
         else {
-            $obj['pid'] = $o['pid'];
+            return false;
         }
 
-        if (gettype($o['data']) != 'array') return false;
+        /**
+         * ProcessId should of type integer
+         */
+        if ($this->isInt($o['pid'], '', $false)) {
+            $obj['pid'] = $o['pid'];
+        }
+        else {
+            return false;
+        }
+
+        list($pid, $did) = [$obj['pid'], $obj['did']];
+
+        /**
+         * Data should be of type array
+         */
+        if (gettype($o['data']) != 'array') {
+            return false;
+        }
         $o = $o['data'];
 
-        if (gettype($o['hasImage']) != 'boolean') return false;
+        /*
+         * Filter image name and delete old one if necessary
+         */
+        if (gettype($o['hasImage']) != 'boolean') {
+            return false;
+        }
         $data['hasImage'] = $o['hasImage'];
 
-        if ($data['hasImage'] && !$this->filterImageName($o['imageName'], 'imageName', $data)) return false;
-        if (gettype($o['control']) != 'array') return false;
+        $hasOldImage = false;
+
+        if (isset($this->oldImages[$pid]) && isset($this->oldImages[$pid][$did])) {
+            $hasOldImage = true;
+        }
+
+        if ($data['hasImage']) {
+            if ($this->filterImageName($o['imageName'], 'imageName', $pid, $did, $data)) {
+                if ($hasOldImage) {
+                    // @TODO: Implement image deletion .
+                }
+                $this->imageNameValidator[$data['imageName']] = 'required|image|mimes:jpeg,png,jpg|max:2048';
+            } else {
+                return false;
+            }
+        } else {
+            if ($hasOldImage) {
+                if (!isset($o['oldImage']) || !$o['oldImage']) {
+                    // @TODO: Implement old image deletion .
+                    $data['hasImage'] = false;
+                    $data['imageName'] = '';
+                } else {
+                    $data['hasImage'] = true;
+                    $data['oldImage'] = true;
+                    $data['imageName'] = $this->oldImages[$pid][$did];
+                }
+            }
+        }
+
+
+        /**
+         * Divide controls into 3 types according to user answers
+         */
+        if (gettype($o['control']) != 'array') {
+            return false;
+        }
 
         $data['control'] = [[], [], []];
         foreach ($o['control'] as $c) {
@@ -71,29 +138,27 @@ class Filter
             $data['control'][$c['value']][] = ['id' => $c['id'], 'value' => $c['value']];
         }
 
+        /*
+         * Filter ploss and udanger
+         */
+        $data['ploss'] = $data['udanger'] = [];
 
-        $data['ploss'] = [];
-        if (gettype($o['ploss']) == 'array') {
-            foreach ($o['ploss'] as $p) {
-                if (!isset($p['id']) || !isset($p['value'])) continue;
-                if (!$this->isInt($p['id'], '', $false) || !$this->isInt($p['value'], '', $false)) continue;
-                if ($p['value'] != 1) continue;
-                $data['ploss'][] = ['id' => $p['id'], 'value' => $p['value']];
+        foreach (['ploss', 'udanger'] as $type) {
+            if (gettype($o[$type]) == 'array') {
+                foreach ($o[$type] as $p) {
+                    if (!isset($p['id']) || !isset($p['value'])) continue;
+                    if (!$this->isInt($p['id'], '', $false) || !$this->isInt($p['value'], '', $false)) continue;
+                    if ($p['value'] != 1) continue;
+                    $data[$type][] = ['id' => $p['id'], 'value' => $p['value']];
+                }
             }
         }
 
-        $data['udanger'] = [];
-        if (gettype($o['udanger']) == 'array') {
-            foreach ($o['udanger'] as $u) {
-                if (!isset($u['id']) || !isset($u['value'])) continue;
-                if (!$this->isInt($u['id'], '', $false) || !$this->isInt($u['value'], '', $false)) continue;
-                if ($u['value'] != 1) continue;
-                $data['udanger'][] = ['id' => $u['id'], 'value' => $u['value']];
-            }
-        }
-
-
-        $types = ['newControls' => 'isString',
+        /**
+         * Filter input[type='text'] fields. Array keys are form names and values - validator functions
+         */
+        $types = [
+            'newControls' => 'isString',
             'newUdangers' => 'isString',
             'rpersons' => 'isString',
             'etimes' => 'isDate'
@@ -103,30 +168,27 @@ class Filter
             $data[$type] = [];
             if (gettype($o[$type]) == 'array') {
                 foreach ($o[$type] as $n) {
-                    if (isset($n['value']))
-                        if (call_user_func([static::class, $filter], $n['value']))
+                    if (isset($n['value'])) {
+                        if (call_user_func([static::class, $filter], $n['value'])) {
                             $data[$type][] = ['value' => $n['value']];
+                        }
+                    }
                 }
             }
         }
-
-
-        //  $data['rperson'] = '';
-        //  if (isset($o['rperson']) && $o['rperson'] != '')
-        //     $data['rperson'] = $o['rperson'];
-
-
-        //  $data['etime'] = '';
-        //  if (isset($o['etime'])){
-        //      if ($this->isDate($o['etime']))  $data['etime'] = $o['etime'];
-        //  }
 
         $obj['data'] = $data;
 
         return $obj;
     }
 
-    public function isInt($a, $name, &$data)
+    /**
+     * @param $a
+     * @param $name
+     * @param $data
+     * @return bool
+     */
+    public function isInt($a, $name, &$data): bool
     {
         if (!preg_match('/^-?\d+$/', $a)) {
             return false;
@@ -136,14 +198,26 @@ class Filter
         return true;
     }
 
-    public function isString($str)
+    /**
+     * @param $str
+     * @return bool
+     */
+    public function isString($str): bool
     {
         return is_string($str);
     }
 
-    public function filterImageName($a, $name, &$data)
+    /**
+     * @param $a
+     * @param $name
+     * @param $pid
+     * @param $did
+     * @param $data
+     * @return bool
+     */
+    public function filterImageName($a, $name, $pid, $did, &$data): bool
     {
-        if (!preg_match('/^(image_[0-9]+_[0-9]+)$/', $a)) {
+        if ($a !== "image_{$pid}_{$did}") {
             return false;
         }
 
@@ -151,7 +225,11 @@ class Filter
         return true;
     }
 
-    public function isDate($d)
+    /**
+     * @param $d
+     * @return bool
+     */
+    public function isDate($d): bool
     {
         if (!preg_match('/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/', $d)) {
             return false;
