0d6289ae265474c78473746e9bc36487866662fa
Vladimer Shatakishvili <gshatakishvili@gmail.com>
2021-01-18 20:46:39 +0400

    Implement create/update functions in Json.php

--- app/Helperclass/Json.php
+++ app/Helperclass/Json.php
@@ -2,40 +2,70 @@
 
 namespace App\Helperclass;
 
-
 use App\Export;
-use Illuminate\Support\Facades\File;
-
 
 class Json {
+    private int $exportId;
 
-    
-    public function __construct(){
-
+    public function __construct($exportId = 0){
+        $this->exportId = $exportId;
     }
 
+    /**
+     * @param $export
+     * @return mixed
+     */
     public static function sload($export){
-        $data = json_decode($export->data, true);
-
-        return $data;
+        return json_decode($export->data, true);
     }
 
+    /**
+     * @param $export
+     * @return mixed
+     */
     public function load($export){
-        // $json = File::get($path);
-        $data = json_decode($export->data, true);
-
-        return $data;
+        return json_decode($export->data, true);
     }
 
-    public function save($data){
+    /**
+     * @param $data
+     * @return int
+     */
+    public function save($data): int
+    {
         $data = json_encode($data);
 
+        if ($this->exportId) {
+            return $this->update($data);
+        }
+
+        return $this->create($data);
+    }
+
+    /**
+     * @param $data
+     * @return mixed
+     */
+    public function create($data) {
         $name = md5(uniqid());
         $path = "$name.json";
-        
+
         $export = Export::create(['user_id' => current_user()->id, 'filename' => $path, 'data' => $data]);
-        // File::put("{$path}", $json);
+
         return $export->id;
+    }
+
+    /**
+     * @param $data
+     * @return int
+     */
+    public function update($data): int
+    {
+        $export = Export::findOrFail($this->exportId);
+
+        $export->update(['data' => $data]);
+
+        return $this->exportId;
     }
 }
 
