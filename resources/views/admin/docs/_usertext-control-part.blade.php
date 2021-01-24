ffb5d0e5e4586ad4b83abe6dacb3783db23b84f2
Vladimer Shatakishvili <gshatakishvili@gmail.com>
2021-01-25 00:01:39 +0400

    Replace fa-remove with fa-trash-alt icon in user/docs

--- resources/views/admin/docs/_usertext-control-part.blade.php
+++ resources/views/admin/docs/_usertext-control-part.blade.php
@@ -10,7 +10,7 @@
        @foreach ($dangers as $key => $d)
            <div class='mt-2 mb-4 mr-2 ml-4' id='danger{{$d[0]->danger_id}}'>
                <a href='danger/{{$d[0]->danger_id}}/edit'>
-                  <h5 class='text-lightblack text-dark text-lowercase py-2 px-0 is-moving'> {{$key}} </h4>
+                  <h5 class='text-lightblack text-dark text-lowercase py-2 px-0 is-moving'> {{$key}} </h5>
                </a>
                @foreach ($d as $ind => $c)
                   <div class='d-flex py-1 px-2 is-moving' id='full-control{{$c->id}}'>
@@ -26,12 +26,12 @@
                                </button>
 
                                <div id='spinner-control{{$c->id}}' class='d-none'>
-                                  <div class='spinner-sm spinner-border text-primary'  style='width:22px;height:22px;'></div>
+                                  <div class='spinner-sm spinner-border text-primary' style='width:22px;height:22px;'></div>
                                </div>
 
                                <button class='mybtn remove' id='remove-button-control{{$c->id}}'
                                        onclick="removeButtonClick({{$c->id}}, 'control', {{$c->danger_id}})">
-                                  <i class='fa fa-remove'></i>
+                                   <i class="fa fa-trash-alt"></i>
                                </button>
 
                                <div id='rspinner-control{{$c->id}}' class='d-none'>
