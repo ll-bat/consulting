8a75a7081ee02351f4e8cd6b29532bf59d61bae1
Vladimer Shatakishvili <gshatakishvili@gmail.com>
2021-01-30 15:26:19 +0400

    Create field form to create/update old one

---
+++ resources/views/admin/_createField.blade.php
@@ -0,0 +1,11 @@
+
+
+<div class="form-group p-2">
+    <input class="form-control modal-input" name="name" placeholder="ახალი სფერო" id="modal-form-input" />
+    <div class="invalid-feedback">
+        <p class="text-danger"> ასეთი სფერო უკვე არსებობს </p>
+    </div>
+    <div class="valid-feedback">
+        <p class="text-success"> სფერო წარმატებით დაემატა </p>
+    </div>
+</div>
