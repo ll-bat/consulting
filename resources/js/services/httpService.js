20a535aea240ce59781281f3a7ab5f6ed62f32d9
Vladimer Shatakishvili <gshatakishvili@gmail.com>
2021-01-30 15:24:20 +0400

    Update httpService.js

--- resources/js/services/httpService.js
+++ resources/js/services/httpService.js
@@ -6,20 +6,44 @@ function run(method, url, data, options = null) {
 }
 
 function errorHandler(status, data) {
+    alert('დაფიქსირდა შეცდომა. სცადეთ თავიდან')
     throw new Error(`Error occurred when processing your request. status - ${status}, body - ${data}`)
 }
 
 const httpService = {
+
+    redirect : false,
+    path : null,
+
+    setup: (obj) => {
+        for (let a in obj) {
+            httpService[a] = obj[a];
+        }
+    },
+
     get : async (url, options) => {
-        const {status, data} = await run('get', url, null, options);
+        const {status, data} = await run('get', url, null, options).catch(err => {
+            if (httpService.redirect) {
+                window.location = httpService.path;
+            } else {
+                errorHandler(status, data)
+                console.log(err);
+            }
+        });
         if (status < STATUS_OK) {
             return data;
         } else {
             errorHandler(status, data)
         }
     },
+
     post: async (url, params, options) => {
-        const {status, data} = await run('post', url, params, options);
+        const {status, data} = await run('post', url, params, options).catch(err => {
+            if (httpService.redirect) {
+                alert('დაფიქსირდა შეცდომა. სცადეთ გვერდის დარეფრეშება');
+                return;
+            }
+        });
         if (status < STATUS_OK) {
             return data;
         } else {
