4ae57e6981f2b9ee1012a44cafa5040c79610a1f
Vladimer Shatakishvili <gshatakishvili@gmail.com>
2021-02-04 22:10:35 +0400

    Change d-cheader.blade.php

--- resources/views/user/docs/doc-header.blade.php
+++ resources/views/user/docs/doc-header.blade.php
@@ -5,7 +5,7 @@
     <tbody>
         <tr>
             <td colspan="2" width="80%" style="padding: 20px 0;">
-                <b> რისკების შეფასების დოკუმენტი </b>
+                <b style="font-size: 1rem"> რისკების შეფასების დოკუმენტი </b>
             </td>
             <td class="text-right font-weight-bold text-sm" style="background: #DEEBF7;">
               <b> დოკუმენტის N:</b>
@@ -15,14 +15,14 @@
             </td>
         </tr>
         <tr style="background: #efeeee">
-            <td width="20%" class="text-sm " style="background: #DEEBF7; padding: 10px 0">
-              <b> შემფასებლის/ების სახელი და გვარი:</b>
+            <td width="20%" class="text-sm" style="background: #DEEBF7; padding: 10px 0">
+              <b class="text-sm"> შემფასებლის/ების სახელი და გვარი:</b>
             </td>
             <td class="text-sm">
                 {{ $docAbout['author-names'] }}
             </td>
             <td class="text-left text-sm font-weight-bold" style="background: #DEEBF7; padding: 10px 0">
-                <b> რისკების შეფასების თარიღი:</b>
+                <b class="text-sm"> რისკების შეფასების თარიღი:</b>
             </td>
             <td class="text-sm">
                 {{ $docAbout['first_date'] }}
@@ -30,13 +30,13 @@
         </tr>
         <tr style="background: #efeeee">
             <td class="text-sm font-weight-bold" style="background: #DEEBF7; padding: 10px 0">
-                <b> სამუშაო ობიექტის დასახელება და მისამართი:</b>
+                <b class="text-sm"> სამუშაო ობიექტის დასახელება და მისამართი:</b>
             </td>
             <td class="text-sm">
                 {{ $docAbout['address'] }}
             </td>
             <td class="text-left text-sm font-weight-bold" style="background: #DEEBF7; padding: 10px 0">
-                <b> დოკუმენტის გადახედვის სავარაუდო თარიღი:</b>
+                <b class="text-sm"> დოკუმენტის გადახედვის სავარაუდო თარიღი:</b>
             </td>
             <td class="text-sm">
                 {{ $docAbout['second_date'] }}
@@ -44,7 +44,7 @@
         </tr>
         <tr style="background: #efeeee">
             <td class="text-sm font-weight-bold" style="background: #DEEBF7; padding: 10px 0">
-                <b> სამუშაოს მოკლე აღწერა: </b>
+                <b class="text-sm"> სამუშაოს მოკლე აღწერა: </b>
             </td>
             <td colspan="3" class="text-sm">
                 {{ $docAbout['description'] }}
