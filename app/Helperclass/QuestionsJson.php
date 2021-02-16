812acd68b119cf3a69cafc2c9aea9ee8a7425ec6
Vladimer Shatakishvili <gshatakishvili@gmail.com>
2021-02-16 21:49:45 +0400

    Update: dump error message if questionsJson has problem

--- app/Helperclass/QuestionsJson.php
+++ app/Helperclass/QuestionsJson.php
@@ -23,7 +23,9 @@ class QuestionsJson
              foreach ($_data as $process) {
                  foreach ($process as $danger) {
 
-                     if (gettype($danger) !== 'object') continue;
+                     if (gettype($danger) !== 'object') {
+                         continue;
+                     }
 
                      list($pid, $did) = [$danger->pid, $danger->did];
 
@@ -49,38 +51,67 @@ class QuestionsJson
                      }
 
                      $data['control'] = [];
+                     $data['newControls'] = [
+                         'first' => [],
+                         'second' => []
+                     ];
 
                      foreach ([0, 1, 2] as $answer) {
+                         $index = 'first';
+                         if ($answer === 1) {
+                             $index = 'second';
+                         }
                          foreach ($danger->control[$answer] as $control) {
-                             $data['control'][] = [
-                                 'id' => $control->id,
-                                 'value' => $answer
-                             ];
+                             if (isset($control->added)) {
+                                 $data['newControls'][$index][] = ['value' => $control->model->name];
+                             } else {
+                                 $data['control'][] = [
+                                     'id' => $control->id,
+                                     'value' => $answer
+                                 ];
+                             }
                          }
                      }
 
                      $data['ploss'] = $data['udanger'] = [];
+                     $data['newPloss'] = $data['newUdangers'] = [];
 
-                     foreach (['ploss', 'udanger'] as $type) {
+                     foreach ([['ploss', 'newPloss'], ['udanger', 'newUdangers']] as [$type, $ref]) {
                          $typeData = $danger->{$type};
                          foreach ($typeData as $val) {
-                             $data[$type][] = [
-                                 'id' => $val->id,
-                                 'value' => 1
-                             ];
+                             if (isset($val->added)) {
+                                 $data[$ref][] = ['value' => $val->model->name];
+                             } else {
+                                 $data[$type][] = [
+                                     'id' => $val->id,
+                                     'value' => 1
+                                 ];
+                             }
+
                          }
                      }
 
-                     foreach (['newControls', 'newUdangers', 'rpersons', 'etimes'] as $type) {
+                     foreach (['rpersons'] as $type) {
                          $typeVal = $danger->{$type};
-                         $data[$type] = $typeVal;
-                         if (!$typeVal) {
-                             $data[$type][] = ['value' => ''];
+                         $data[$type] = $typeVal ?? [];
+                     }
+
+                     $data['etimes'] = [
+                         'normal' => [],
+                         'time' => [],
+                     ];
+
+                     if (isset($danger->etimes)) {
+                         foreach ($danger->etimes as $etime) {
+                             if (isset($etime->type)) {
+                                 $data['etimes'][$etime->type][] = ['value' => $etime->value];
+                             }
                          }
                      }
 
                      $el['data'] = $data;
                      $obj[] = $el;
+
                  }
              }
 
@@ -91,11 +122,13 @@ class QuestionsJson
              }
 
          } catch (\Exception $e) {
-//             dd($e->getMessage() . '; line - ' . $e->getLine());
+             dd($e->getMessage() . '; line - ' . $e->getLine());
+             exit;
              $this->data = [];
          }
 
-         $this->data = $obj;
+//         dd($this->data);
+
      }
 
      public function getData(): array
