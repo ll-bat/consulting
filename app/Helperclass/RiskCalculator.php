d3fca881647d162c323cdc0569872c20f5e93c60
Vladimer Shatakishvili <gshatakishvili@gmail.com>
2021-02-16 21:48:46 +0400

    Update: change risk's calculation

--- app/Helperclass/RiskCalculator.php
+++ app/Helperclass/RiskCalculator.php
@@ -4,101 +4,200 @@ namespace App\Helperclass;
 
 use App\Control;
 
-class RiskCalculator {
+class RiskCalculator
+{
 
     private $result = [];
     private $reducesPotentialLoss = false;
 
-    public function __construct($k, $obj){
+    /**
+     * $k is the current danger's k
+     * $obj includes [ploss, controls, sumOfAllControlsInDanger]
+     *
+     * RiskCalculator constructor.
+     * @param $k
+     * @param $obj
+     */
+    public function __construct($k, $obj)
+    {
         $this->calculate(dval($k), $obj);
     }
 
-    public function calculate($k, $obj){
+    /**
+     * Do risk's calculation
+     *
+     * @param $k
+     * @param $obj
+     */
+    public function calculate($k, $obj)
+    {
         $this->calcFirstResult($k, $obj);
         $this->calcFirstProbability($k, $obj);
         $this->calcFirstLevel();
-        $this->calcSecondResult();
-        $this->calcSecondProbability();
+        /**
+         * calcFirstResult calculates second[result, probability] as well, due to some calculated var..
+         */
         $this->calcSecondLevel();
     }
 
-    public function calcFirstResult($k, $obj){
+    /**
+     * Danger k is multiplied by the max value found among the checked potentialLosses' k.
+     *
+     * @param $k
+     * @param $obj
+     */
+    public function calcFirstResult($k, $obj)
+    {
         $mx = 0;
-        $test = [];
-        foreach ($obj['ploss'] as $p){
+        foreach ($obj['ploss'] as $p) {
             $mx = max($mx, dval($p['model']['k']));
-            $test[] = dval($p['model']['k']);
         }
 
         $result = round($mx * $k);
         if ($result > 5) $result = 5;
         elseif ($result < 1) $result = 1;
 
-        $test[] = $k;
-        $test[] = $result;
-
         $this->result['first_result'] = $result;
-        // dd($test);
     }
 
-    public function calcFirstProbability($k, $obj){
-        $userControlsSum = 0;
-        $test = [];
-        foreach ($obj['control'][0] as $c){
-            $userControlsSum += dval($c['model']['k']);
-            $test['userControlsK'][] = dval($c['model']['k']);
-        }
+    public function calcFirstProbability($k, $obj)
+    {
+        $firstControlsSum = 0;
+        $secondControlsSum = 0;
+        $addedControlsSum = 0;
 
-        foreach ($obj['control'][1] as $c){
-            if ($c['model']['rploss'] == 1){
-                $this->reducesPotentialLoss = true;
-                break;
+        /**
+         * Calculate [first] type controls sum and $addedControls as well.
+         */
+        foreach ($obj['control'][0] as $c) {
+            $k = dval($c['model']['k']);
+
+            $firstControlsSum += $k;
+            if (isset($c['added'])) {
+                $addedControlsSum += $k;
             }
         }
 
-        $allControlsSum = $obj['dangerControlsSum'];
+        /**
+         * Calculate $secondControlsSum and find if any of the chosen potentialLosses has 'rploss' checked.
+         */
 
-        $percent = ($userControlsSum / $allControlsSum) * 100;
-        $test['percent'] = $percent;
+        foreach ($obj['control'][1] as $c) {
+            $k = dval($c['model']['k']);
 
-        $types = [20 => 5,40 => 4,60 => 3, 80 => 2, 101 => 1];
+            $secondControlsSum += $k;
 
-        $result = -1;
-        foreach ($types as $type => $val){
-             if ($percent >= $type) continue;
-             $result = $val;
-             break;
+            if (isset($c['added'])) {
+                $addedControlsSum += $k;
+            }
+
+            if ($c['model']['rploss'] == 1) {
+                $this->reducesPotentialLoss = true;
+            }
         }
 
-        if ($result == -1) $result = 1;
+        /**
+         * all controls some;
+         */
+        $allControlsSum = $obj['dangerControlsSum'] + $addedControlsSum;
 
-        $test['result'] = $result;
+        /**
+         * percent of the [first] controls type;
+         */
+        $percent = ($firstControlsSum / $allControlsSum) * 100;
 
-        $this->result['first_probability'] = $result;
+        /**
+         * small diagram like structure which gives us a 'score' according to percent;
+         */
+        $scores = [10 => 5, 30 => 4, 50 => 3, 70 => 2, 101 => 1];
 
-        // dd($test);
-    }
+        /**
+         * For easy understandings, let's simulate the score calculation process.
+         * Say our percent is 30, then we have:
+         * Iteration one: $currentPercent = 10, $score = 5    -> omit this element;
+         * Iteration two: $currentPercent = 30, $score = 4    -> still omit this element, because score 3 is given to those having probability in range [30, 49.9];
+         * Iteration three: $currentPercent = 50, $score = 3  -> This time, comparison returns false, so we get score - 3;
+         *
+         * This piece of code is needed for calculating second_result as well
+         * Therefore, we create a function which accepts one - $percent - argument, and returns - $score
+         *
+         * @param $ourPercent
+         * @return int
+         */
 
-    public function calcFirstLevel(){
-        $this->result['first_level'] = $this->result['first_result'] * $this->result['first_probability'];
-        // dd($this->result['first_level']);
-    }
+        $getScore = function($ourPercent) use($scores) {
+            $result = -1;
+            foreach ($scores as $currentPercent => $score) {
+                if ($ourPercent >= $currentPercent) continue;
+                $result = $score;
+                break;
+            }
 
-    public function calcSecondResult(){
+            if ($result < 1) {
+                $result = 1;
+
+                /**
+                 * TODO: Log error. Something does not work properly.
+                 */
+            }
+
+            return $result;
+        };
+
+        /**
+         * We now have first probability.
+         */
+        $this->result['first_probability'] = $getScore($percent);
+
+
+        /**
+         * This time we should work on second[result, level] calculation
+         * We can do the rest in another function, but
+         * variables are already calculated, so we can actually calculate them.
+         */
+
+        /**
+         * First, calculate new percent
+         */
+        $percent = ( ($firstControlsSum + $secondControlsSum) / $allControlsSum ) * 100;
+
+        /**
+         * Scores are the same, so we keep them.
+         * Use $getScore function to calculate [secondProbability];
+         */
+
+        $this->result['second_probability'] = $getScore($percent);
+
+        /**
+         * Extra risk's result is almost same as [first] risk's result,
+         * the only difference is that, this one reduces by 1, if any of the potentialLosses has checked - 'Reduces potential loss'
+         */
+
         $this->result['second_result'] = $this->result['first_result'];
-        if ($this->reducesPotentialLoss && $this->result['second_result'] > 1) $this->result['second_result']--;
-        // dd($this->reducesPotentialLoss);
+        if ($this->reducesPotentialLoss && $this->result['second_result'] > 1) {
+            $this->result['second_result']--;
+        }
+
     }
 
-    public function calcSecondProbability() {
-        $this->result['second_probability'] = $this->result['first_probability'];
+    public function calcFirstLevel()
+    {
+        $this->result['first_level'] = $this->result['first_result'] * $this->result['first_probability'];
     }
 
-    public function calcSecondLevel(){
+
+    public function calcSecondLevel()
+    {
         $this->result['second_level'] = $this->result['second_result'] * $this->result['second_probability'];
     }
 
-    public function getResult(){
+    /**
+     * Returns calculated risks.
+     *
+     * @return array
+     */
+    public function getResult()
+    {
         return $this->result;
     }
 
