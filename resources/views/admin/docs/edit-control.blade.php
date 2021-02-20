4315233cfd0e78ca922a1d95dbf5ceb613cd4802
Vladimer Shatakishvili <gshatakishvili@gmail.com>
2021-02-20 17:18:56 +0400

    Reformat code

--- resources/views/admin/docs/edit-control.blade.php
+++ resources/views/admin/docs/edit-control.blade.php
@@ -37,110 +37,115 @@
         .my-bg {
             background: #009999;
         }
-   </style>
+    </style>
 @endsection
 
 <?php $color = '#009999'; ?>
 
 @section('content')
 
-<div class="zim-container">
+    <div class="zim-container">
         <div class="row justify-content-center">
 
             <div class="col-xl-6 col-lg-7 col-md-10 col-sm-12 col-12 ml-5 mr-5" id="main-part">
 
                 @if (Session('message'))
-                   <p class='alert my-bg text-left text-white mt-2 mb-3'>
-                         {{Session('message')}}
-                   </p>
+                    <p class='alert my-bg text-left text-white mt-2 mb-3'>
+                        {{Session('message')}}
+                    </p>
                 @endif
 
                 <form method='post' action='update'>
-                   @csrf
+                    @csrf
 
-                      <div class='card border-0 partial-shadow' style=''>
-                             <div class='d-flex'>
-                                 <div class='mx-3 mt-3 w-75'>
-                                    <div class='form-group'>
+                    <div class='card border-0 partial-shadow' style=''>
+                        <div class='d-flex'>
+                            <div class='mx-3 mt-3 w-75'>
+                                <div class='form-group'>
                                           <textarea type='text' class='form-control autoresize'
-                                                placeholder='დაამატეთ კონტროლის ზომა'
-                                                oninput="$(window).trigger('autoresize')"
-                                                name='name'>{{$control->name}}</textarea>
-                                    </div>
-                                    @error('name')
-                                       <p class='text-sm text-danger text-left'> {{$message}} </p>
-                                    @enderror
-                                 </div>
-                                 <div class='mx-3 mt-3 text-left'>
-                                       <input type='text' class='form-control border-0 autoresize'
-                                              placeholder='K:1'
-                                              name='k'
-                                              value = "{{$control->k}}"
-                                              style='width:4rem;border-bottom:1px solid {{$color}} !important;border-radius:0' name='k' />
-                                 </div>
-                             </div>
-
-                             <label class="ns-container text-secondary text-left ml-3 mt-2 mb-3" style='font-size:.95em; color:rgba(0,0,0,.8);'> ამცირებს პოტენციურ ზიანს
-                                <input type="checkbox"
-                                       name="rploss"
-                                       value="1"
-                                       @if ($control->hasRPLoss()) checked @endif
-                                >
-                                <span class="chbox-checkmark"></span>
-                           </label>
-                      </div>
-
-                      <div class='card text-left border-0 partial-shadow'>
-                           <div class='card-body ml-1 pb-4' required>
-                                <p class='ml-1 mt-2 mb-4 font-weight-bolder my-color'> აირჩიეთ საფრთხე </p>
-
-                                @foreach($dangers as $danger)
-                                <label class="ns-container mt-3 ml-3 text-secondary" style='font-size:.95em; color:rgba(0,0,0,.8);'>{{$danger->name}}
-                                     <input type="checkbox"
-                                            name="danger[]"
-                                            value="{{ $danger->id }}"
-                                            @if(in_array($danger->id, $list)) checked @endif
-                                     >
-                                     <span class="chbox-checkmark"></span>
-                                </label>
-                                @endforeach
-
-                                @if ($dangers->count() == 0)
-                                   <p class='text-secondary font-weight-bolder ml-3'> საფრთხეები არ არის </p>
-                                @endif
-
-                                @error('danger')
-                                     <p class='text-sm text-danger mt-2 mb-0 pb-0'> {{$message}} </p>
+                                                    placeholder='დაამატეთ კონტროლის ზომა'
+                                                    oninput="$(window).trigger('autoresize')"
+                                                    name='name'>{{$control->name}}</textarea>
+                                </div>
+                                @error('name')
+                                <p class='text-sm text-danger text-left'> {{$message}} </p>
                                 @enderror
-
-                               <hr style="width: 100%" />
-
-                               <div class='d-md-flex d-block'>
-                                   <div class='text-left'>
-                                       <button class='btn my-btn'> განახლება </button>
-                                   </div>
-
-                                   <div class='text-left ml-md-2 ml-0'>
-                                       <button class='btn my-del-btn' onclick="event.preventDefault();$1('control-delete').submit(); "> წაშლა </button>
-                                   </div>
-                               </div>
                             </div>
-                      </div>
+                            <div class='mx-3 mt-3 text-left'>
+                                <input type='text' class='form-control border-0 autoresize'
+                                       placeholder='K:1'
+                                       name='k'
+                                       value="{{$control->k}}"
+                                       style='width:4rem;border-bottom:1px solid {{$color}} !important;border-radius:0'
+                                       name='k'/>
+                            </div>
+                        </div>
+
+                        <label class="ns-container text-secondary text-left ml-3 mt-2 mb-3"
+                               style='font-size:.95em; color:rgba(0,0,0,.8);'> ამცირებს პოტენციურ ზიანს
+                            <input type="checkbox"
+                                   name="rploss"
+                                   value="1"
+                                   @if ($control->hasRPLoss()) checked @endif
+                            >
+                            <span class="chbox-checkmark"></span>
+                        </label>
+                    </div>
+
+                    <div class='card text-left border-0 partial-shadow'>
+                        <div class='card-body ml-1 pb-4' required>
+                            <p class='ml-1 mt-2 mb-4 font-weight-bolder my-color'> აირჩიეთ საფრთხე </p>
+
+                            @foreach($dangers as $danger)
+                                <label class="ns-container mt-3 ml-3 text-secondary"
+                                       style='font-size:.95em; color:rgba(0,0,0,.8);'>{{$danger->name}}
+                                    <input type="checkbox"
+                                           name="danger[]"
+                                           value="{{ $danger->id }}"
+                                           @if(in_array($danger->id, $list)) checked @endif
+                                    >
+                                    <span class="chbox-checkmark"></span>
+                                </label>
+                            @endforeach
+
+                            @if ($dangers->count() == 0)
+                                <p class='text-secondary font-weight-bolder ml-3'> საფრთხეები არ არის </p>
+                            @endif
+
+                            @error('danger')
+                            <p class='text-sm text-danger mt-2 mb-0 pb-0'> {{$message}} </p>
+                            @enderror
+
+                            <hr style="width: 100%"/>
+
+                            <div class='d-md-flex d-block'>
+                                <div class='text-left'>
+                                    <button class='btn my-btn'> განახლება</button>
+                                </div>
+
+                                <div class='text-left ml-md-2 ml-0'>
+                                    <button class='btn my-del-btn'
+                                            onclick="event.preventDefault();$1('control-delete').submit(); "> წაშლა
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
 
                 </form>
                 <form method='post' action='rdelete' id='control-delete'>
-                                   @csrf
-                                   @method('delete')
+                    @csrf
+                    @method('delete')
                 </form>
 
-           </div>
+            </div>
+        </div>
     </div>
-</div>
 
-<script type="application/javascript">
+    <script type="application/javascript">
 
 
-</script>
+    </script>
 
 @endsection
--- resources/views/admin/docs/edit-danger.blade.php
+++ resources/views/admin/docs/edit-danger.blade.php
@@ -32,11 +32,21 @@
             background: #ee5c2d !important;
             color: white;
         }
+
+        .new-btn {
+            background: white;
+            border: 1px solid #007799 !important;
+            color: #007799 !important;
+        }
+
+        .new-btn:hover {
+            background: #007799 !important;
+        }
     </style>
 @endsection
 
 <?php $color = '#009999';
-      $delcolor = '#ee5c2d';
+$delcolor = '#ee5c2d';
 ?>
 
 @section('content')
@@ -45,173 +55,99 @@
         <div class="row justify-content-center">
             <div class="col-xl-6 col-lg-8 col-md-10 col-sm-12 col-12 ml-5 mr-5" id="main-part">
 
-            @if (Session('message'))
-                <p class='alert text-left text-white mb-4' style="background: #009999">
-                    {{Session('message')}}
-                </p>
-            @endif
+                @if (Session('message'))
+                    <p class='alert text-left text-white mb-4' style="background: #009999">
+                        {{Session('message')}}
+                    </p>
+                @endif
 
-            @if (Session('error'))
-                <p class='alert alert-danger text-white text-left'>
-                    {{Session('error')}}
-                </p>
-            @endif
+                @if (Session('error'))
+                    <p class='alert alert-danger text-white text-left'>
+                        {{Session('error')}}
+                    </p>
+                @endif
 
-            <form method='post' action='update' id="form-update">
-                @csrf
+                <div class="text-right">
+                    <button class="btn btn-outline-primary new-btn px-3 py-1" id="minimize-btn">
+                        <i class="fa fa-minus"></i>
+                    </button>
+                </div>
 
-                <div class='card border-0 partial-shadow p-1' style=''>
-                    <div class='d-flex'>
-                        <div class='mx-2 mt-2 w-75'>
-                            <div class='form-group'>
+                <form method='post' action='update' id="form-update">
+                    @csrf
+
+                    <div class='card border-0 partial-shadow p-1' style=''>
+                        <div class='d-flex'>
+                            <div class='mx-2 mt-2 w-75'>
+                                <div class='form-group'>
                                     <textarea type='text'
                                               class='form-control autoresize'
                                               placeholder='დაამატეთ საფრთხე'
                                               oninput="$(window).trigger('autoresize')"
-                                              name='name'>{{ $danger->name }}</textarea>
+                                              name='name'>{{ $danger->name }}
+                                    </textarea>
+                                </div>
+                                @error('name')
+                                <p class='text-sm text-danger text-left'> {{$message}} </p>
+                                @enderror
                             </div>
-                            @error('name')
-                            <p class='text-sm text-danger text-left'> {{$message}} </p>
+                            <div class='mx-2 mt-2 text-left'>
+                                <input type='text' class='form-control border-0'
+                                       placeholder='K:1'
+                                       name='k'
+                                       value="{{$danger->k}}"
+                                       style='width:3.5rem;border-bottom:1px solid orange !important;border-radius:0'
+                                       name='k'/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class='card border-0 partial-shadow pb-2' id="choose-process"
+                         style=''>
+                        <div class="card-body text-left p-3">
+                            <p class='mt-2 mb-4 font-weight-bolder' style="color: #009999"> აირჩიეთ პროცესი </p>
+                            <div class="mt-4">
+                                @foreach($procs as $proc)
+                                    <label class="ns-container mt-3 ml-2 text-secondary"
+                                           style='font-size:.95em; color:rgba(0,0,0,.8);'>{{$proc->name}}
+                                        <input type="checkbox"
+                                               name="process[]"
+                                               value="{{ $proc->id }}"
+                                               @if(in_array($proc->id, $list)) checked @endif
+                                        >
+                                        <span class="chbox-checkmark" style=""></span>
+                                    </label>
+                                @endforeach
+                            </div>
+
+                            @if ($procs->count() == 0)
+                                <p class='text-secondary pl-3'> პროცესები არ არის </p>
+                            @endif
+
+                            @error('process')
+                            <p class='text-sm text-danger mt-2 mb-0 pb-0'> {{$message}} </p>
                             @enderror
                         </div>
-                        <div class='mx-2 mt-2 text-left'>
-                            <input type='text' class='form-control border-0'
-                                   placeholder='K:1'
-                                   name='k'
-                                   value="{{$danger->k}}"
-                                   style='width:3.5rem;border-bottom:1px solid orange !important;border-radius:0'
-                                   name='k'/>
+
+                        <hr style="width:94%;margin-left:3%;"/>
+
+                        <div class='d-flex m-2 mx-3'>
+                            <div class=''>
+                                <button class='btn my-btn' onclick="updateHandler(this)"> განახლება</button>
+                            </div>
+
+                            <div class='ml-2'>
+                                <button class='btn my-del-btn' style='border-width: 1px !important'
+                                        onclick="event.preventDefault();$1('danger-delete').submit(); "> წაშლა
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
-
-                <div class='card border-0 partial-shadow pb-2 '
-                     style=''>
-                    <div class="card-body text-left p-3">
-                        <p class='mt-2 mb-4 font-weight-bolder' style="color: #009999"> აირჩიეთ პროცესი </p>
-                        <div class="mt-4">
-                            @foreach($procs as $proc)
-                                <label class="ns-container mt-3 ml-2 text-secondary"
-                                       style='font-size:.95em; color:rgba(0,0,0,.8);'>{{$proc->name}}
-                                    <input type="checkbox"
-                                           name="process[]"
-                                           value="{{ $proc->id }}"
-                                           @if(in_array($proc->id, $list)) checked @endif
-                                    >
-                                    <span class="chbox-checkmark" style=""></span>
-                                </label>
-                            @endforeach
-                        </div>
-
-                        @if ($procs->count() == 0)
-                            <p class='text-secondary pl-3'> პროცესები არ არის </p>
-                        @endif
-
-                        @error('process')
-                        <p class='text-sm text-danger mt-2 mb-0 pb-0'> {{$message}} </p>
-                        @enderror
-                    </div>
-
-                    <hr style="width:94%;margin-left:3%;"/>
-
-                    <div class='d-flex m-2 mx-3'>
-                        <div class=''>
-                            <button class='btn my-btn' onclick="updateHandler(this)"> განახლება</button>
-                        </div>
-
-                        <div class='ml-2'>
-                            <button class='btn my-del-btn' style = 'border-width: 1px !important'
-                                    onclick="event.preventDefault();$1('danger-delete').submit(); "> წაშლა
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </form>
-            <form method='post' action='delete' id='danger-delete'>
-                @csrf
-                @method('delete')
-            </form>
-
-
-            {{--            <div class='card mt-4 text-left ns-font-family  shadow-none rounded-10 ns-border-bottom'--}}
-            {{--                 style='border-top:10px solid blue'>--}}
-            {{--                <h4 class='p-3'> ყველა შემავალი კონტროლის ზომა </h4>--}}
-            {{--                <div class='card-body pl-2 pt-2'>--}}
-            {{--                    @foreach ($ycontrol as $ind => $d)--}}
-            {{--                        <div class='d-flex my-2'>--}}
-            {{--                            <div style='width:85%;'>--}}
-            {{--                                <a href='../../control/{{$d->id}}/edit' class='mt-1 pb-2 pl-2 text-muted'>--}}
-            {{--                                    <b>{{$ind + 1}}.</b> {{$d->name}} </a>--}}
-            {{--                            </div>--}}
-            {{--                            <div style='width:15%'>--}}
-            {{--                                <a href='edit/{{$d->id}}/detach'--}}
-            {{--                                   class='btn btn-outline-danger text-danger text-sm rounded-pill capitalize px-md-3 px-0 py-1'--}}
-            {{--                                   style='font-size:.7em;min-width:70px !important;'> Uncheck </a>--}}
-            {{--                            </div>--}}
-            {{--                        </div>--}}
-            {{--                    @endforeach--}}
-            {{--                </div>--}}
-            {{--            </div>--}}
-
-
-{{--            <ul class="list-group text-left mt-5 mb-4" style="border-radius:5px;">--}}
-{{--                <li class="list-group-item font-weight-bold py-3 pointer"--}}
-{{--                    onclick="toggleCollapseHandler(this, 'all-in-controls-panel', '#009999')"--}}
-{{--                    style="background-color: #009999;border: 1px solid #009999;color: white;">--}}
-{{--                    <i class='fa fa-plus float-left'></i>--}}
-{{--                    <span class="pl-4"> ყველა შემავალი კონტროლის ზომა </span>--}}
-{{--                </li>--}}
-{{--                @foreach($ycontrol as $ind => $d)--}}
-{{--                    <li class="list-group-item all-in-controls-panel pl-4"--}}
-{{--                        style="border:none;border-bottom: 1px solid rgba(0,0,0,.055);--}}
-{{--                            border-left: 1px solid rgba(0,0,0,0.09);--}}
-{{--                            border-right: 1px solid rgba(0,0,0,.09);--}}
-{{--                        @if ($ind == count($ycontrol)-1) border-bottom: 1px solid rgba(0,0,0,0.1); @endif--}}
-{{--                            ">--}}
-{{--                        <div class="row">--}}
-{{--                            <div class="col-md-9 col-lg-9 col-xl-10 col-12">--}}
-{{--                                <a href='../../control/{{$d->id}}/edit' class='mt-1 pb-2 pl-2 text-muted'>--}}
-{{--                                    <b>{{$ind + 1}}.</b> {{$d->name}} </a>--}}
-{{--                            </div>--}}
-{{--                            <div class="col-md-3 col-lg-3 col-xl-2 col-2 this-div">--}}
-{{--                                <a href='edit/{{$d->id}}/detach'--}}
-{{--                                   class='text-sm capitalize px-md-3 px-0 py-1'--}}
-{{--                                   style='font-size:.8em;color:indianred '> ამოშლა </a>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </li>--}}
-{{--                @endforeach--}}
-{{--            </ul>--}}
-
-
-{{--            <ul class="list-group text-left mt-0 mb-4" style="border-radius:5px;">--}}
-{{--                <li class="list-group-item font-weight-bold py-3 pointer"--}}
-{{--                    onclick="toggleCollapseHandler(this, 'controls-panel', '{{$color}}')"--}}
-{{--                    style="border: 1px solid {{$color}}; color: {{$color}};">--}}
-{{--                    <i class='fa fa-plus float-left'></i>--}}
-{{--                    <span class="pl-4"> ყველა სხვა კონტროლის ზომა </span>--}}
-{{--                </li>--}}
-{{--                @foreach($ncontrol as $ind => $d)--}}
-{{--                    <li class="list-group-item pl-4 controls-panel d-none"--}}
-{{--                        style="border:none;border-bottom: 1px solid rgba(0,0,0,.055);--}}
-{{--                            border-left: 1px solid rgba(0,0,0,0.09);--}}
-{{--                            border-right: 1px solid rgba(0,0,0,.09);--}}
-{{--                        @if ($ind == count($ycontrol)-1) border-bottom: 1px solid rgba(0,0,0,0.1); @endif--}}
-{{--                            ">--}}
-{{--                        <div class="row">--}}
-{{--                            <div class="col-md-10 col-12">--}}
-{{--                                <a href='../../control/{{$d->id}}/edit' class='mt-1 pb-2 pl-2 text-muted'>--}}
-{{--                                    <b>{{$ind + 1}}.</b> {{$d->name}} </a>--}}
-{{--                            </div>--}}
-{{--                            <div class="col-md-2 col-2 text-md-center text-left this-div">--}}
-{{--                                <a href='edit/{{$d->id}}/attach'--}}
-{{--                                   class='text-sm capitalize px-md-3 px-0 py-1'--}}
-{{--                                   style='font-size:.8em;color:#009999 '> დამატება </a>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </li>--}}
-{{--                @endforeach--}}
-{{--            </ul>--}}
+                </form>
+                <form method='post' action='delete' id='danger-delete'>
+                    @csrf
+                    @method('delete')
+                </form>
 
                 @include('admin.docs._docs_tables', [
                           'has' => $ycontrol,
@@ -221,34 +157,53 @@
                           'delcolor' => $delcolor,
                           'typeName' => 'კონტროლის ზომა'
                         ])
+
+                    <ul class="list-group text-left mb-4" style="border-radius:5px;">
+                        <li class="list-group-item font-weight-bold py-3 pointer"
+                            style="background: #2159a2; color: white"
+                        >
+                            <i class='nc-icon nc-single-02 float-left'></i>
+                            <span class="pl-4"> მომხმარებელთა მიერ დამატებულები  </span>
+                        </li>
+                        <li class="list-group-item pl-4 py-4">
+                            <a href="control"> 1. კონტროლის ზომები ({{ $controlsCount }}) </a>
+                        </li>
+                        <li class="list-group-item pl-4 py-4">
+                            <a href="ploss"> 2. პოტენციური ზიანი ({{ $plossCount }}) </a>
+                        </li>
+                        <li class="list-group-item pl-4 py-4">
+                            <a href="udanger"> 3. ვინ იმყოფება საფრთხის ქვეშ ({{ $udangerCount }}) </a>
+                        </li>
+                    </ul>
+            </div>
         </div>
-    </div>
 
-    <script type="application/javascript">
-        function toggleCollapseHandler(obj, clname, bgc) {
-            if ($(`.${clname}`).hasClass('d-none')) {
-                $(`.${clname}`).removeClass('d-none');
-                $(obj).css({'background-color': bgc, 'color': 'white'})
-            } else {
-                $(`.${clname}`).addClass('d-none')
-                $(obj).css({'background-color': 'white', 'color': bgc})
+        <script type="application/javascript">
+            function toggleCollapseHandler(obj, clname, bgc) {
+                if ($(`.${clname}`).hasClass('d-none')) {
+                    $(`.${clname}`).removeClass('d-none');
+                    $(obj).css({'background-color': bgc, 'color': 'white'})
+                } else {
+                    $(`.${clname}`).addClass('d-none')
+                    $(obj).css({'background-color': 'white', 'color': bgc})
+                }
             }
-        }
 
-        function updateHandler(elem){
+            $(document).ready(() => {
+                const el = dom.querySelector('#minimize-btn');
+                const fa = el.querySelector('.fa');
+                const p = $('#choose-process');
+                $(el).on('click', (e) => {
+                    if ($(fa).hasClass('maximized')) {
+                        fa.className = 'fa fa-minus';
+                    } else {
+                        fa.className = 'fa fa-plus maximized';
+                    }
 
-        }
+                    p.toggleClass('d-none');
+                });
+            })
 
-        // window.addEventListener('resize', (e) => {
-        //     if (window.innerWidth < 1600) {
-        //         $('#main-column').removeClass('col-xl-6').addClass('col-xl-7')
-        //     } else {
-        //         $('#main-column').removeClass('col-xl-7').addClass('col-xl-6')
-        //     }
-        // })
-        //
-        // window.dispatchEvent(new Event('resize'))
-
-    </script>
+        </script>
 
 @endsection
