3517dc0987f50490d3cca341d2cf6509c18b7ca5
Vladimer Shatakishvili <gshatakishvili@gmail.com>
2021-03-08 17:53:34 +0400

    Change date format and add new Method to Export.php

--- app/Export.php
+++ app/Export.php
@@ -7,14 +7,20 @@ use Illuminate\Support\Facades\Storage;
 
 class Export extends Model
 {
-      public $table = 'exports';
-      protected $fillable = ['user_id', 'object_id', 'filename', 'data', 'field_id', 'author-names', 'address', 'description', 'first_date', 'second_date', 'number'];
+    public $table = 'exports';
+    protected $fillable = ['user_id', 'object_id', 'filename', 'data', 'field_id', 'author-names', 'address', 'description', 'first_date', 'second_date', 'number'];
 
-      public function path(){
-          return 'exports/'.$this->filename;
-      }
+    public function path()
+    {
+        return 'exports/' . $this->filename;
+    }
 
-      public function dateCreated(){
-        return  $this->created_at->format('yy-M-d');
+    public function dateCreated()
+    {
+        return $this->created_at->format('d M yy');
+    }
+
+    public function dateModified() {
+        return $this->updated_at->format('d M yy');
     }
 }
